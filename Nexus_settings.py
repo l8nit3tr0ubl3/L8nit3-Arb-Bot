4608dbf11166ffc2ebce4fb4336768bf67bf678c
L8nit3tr0ubl3 <l8nit3@outlook.com>
2020-06-06 12:37:40 -0400

    Update Nexus_settings.py

--- Nexus_settings.py
+++ Nexus_settings.py
@@ -1,15 +1,14 @@
-BITTREX_API_KEY = ''
-BITTREX_API_SECRET = ''
-BINANCE_API_KEY = ''
-BINANCE_API_SECRET = ''
+BITTREX_API_KEY = 'API_KEY_HERE'
+BITTREX_API_SECRET = 'API_SECRET_HERE'
+BINANCE_API_KEY = 'API_KEY_HERE'
+BINANCE_API_SECRET = 'API_SECRET_HERE'
 COIN = 'NXS' #Coin to use
-COIN_PAIR_BITTREX = 'BTC-NXS' #Pair at bittrex
-COIN_PAIR_BINANCE = 'NXSBTC' #Pair at binance
-AMOUNT_TO_SELL = 50 #Amount of chosen coin to sell each trade
-DESIRED_PERCENT_GAIN = 1.5 #Desired gain every trade
-DEBUG = 0 #0=off 1=stats 2=stats+errors
-DRY_RUN = True #No withdrawl or trades, just test
+AMOUNT_TO_TRADE = 290 #Amount of chosen coin to sell each trade
+DESIRED_PERCENT_GAIN = 4 #Desired gain every trade
+DEBUG = 1 #0=off 1=stats 2=stats+errors
+DRY_RUN = False #No withdrawl or trades, just test
 ENABLE_WITHDRAWLS = False #trade, but dont auto-equalize balances
-GAIN_BTC = False #True = stack sats, False = stack coin
+GAIN_BTC = True #True = stack sats, False = stack coin
 SLEEP_BETWEEN_CYCLE = 3 #Sleep between price checks
+DESIRED_CYCLES = 1 #Number of trades before equalize
 SLEEP_AFTER_TRADE = 300 #Sleep after trade
